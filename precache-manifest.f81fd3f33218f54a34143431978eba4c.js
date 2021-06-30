9188060a47d7f1f3b0cbc4f406f65e245e9f5980
Burulai <burulainarzieva2@gmail.com>
2021-07-01 00:11:28 +0600

    Updates

---
+++ precache-manifest.f81fd3f33218f54a34143431978eba4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3a7ed70b97b5d7541671dfc56f61e5",
+    "url": "/Pomodoro-app/index.html"
+  },
+  {
+    "revision": "9ef9e0f44a49456b1aa0",
+    "url": "/Pomodoro-app/static/css/main.f9f168f2.chunk.css"
+  },
+  {
+    "revision": "ccd29bca326eea62401c",
+    "url": "/Pomodoro-app/static/js/2.de9c8c41.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/Pomodoro-app/static/js/2.de9c8c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef9e0f44a49456b1aa0",
+    "url": "/Pomodoro-app/static/js/main.2e7f602c.chunk.js"
+  },
+  {
+    "revision": "a996b11765878bf0e33c",
+    "url": "/Pomodoro-app/static/js/runtime-main.6aef5761.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro-app/static/media/bell.525320df.flac"
+  }
+]);
